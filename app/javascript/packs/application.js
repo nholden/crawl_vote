a2aaa711d879662591d8eb58ba8c6a988a646a79
Nick Holden <nick.r.holden@gmail.com>
2018-05-08 10:12:17 -0700

    Include Turbolinks via Webpacker instead of the asset pipeline

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,11 @@
+import Turbolinks from 'turbolinks';
 import TurbolinksAdapter from 'vue-turbolinks';
 import Vue from 'vue/dist/vue.esm'
 import CrawlContainer from '../CrawlContainer'
 import VueClipboard from 'vue-clipboard2'
 import '../stylesheets/application.scss'
+
+Turbolinks.start()
 
 Vue.use(TurbolinksAdapter)
 Vue.use(VueClipboard)
