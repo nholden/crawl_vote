8bf9e34c17aff3fe1da2d294bb93a730611da47e
Nick Holden <nick.r.holden@gmail.com>
2018-05-20 17:43:09 -0700

    Rename app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "where_to_crawl_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "crawl_vote_#{Rails.env}"
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
