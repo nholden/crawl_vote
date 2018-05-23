d9d26e097c28f89d26cff1366041cb8813d0fb94
Nick Holden <nick.r.holden@gmail.com>
2018-05-23 08:31:32 -0700

    Copy updates

--- app/javascript/JoinCrawlForm.vue
+++ app/javascript/JoinCrawlForm.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
 form
   .pb-4
-    label.block.pb-1.font-medium.text-sm.text-blue-light.uppercase.tracking-wide(for="token") Crawl code
+    label.block.pb-1.font-medium.text-sm.text-blue-light.uppercase.tracking-wide(for="token") Code
     input.shadow.appearance-none.border.rounded.w-full.py-2.px-3(type="text" id="token" v-model="token")
   .pt-2
     input.btn.btn--active.w-full(type="submit" value="Join Crawl" v-on:click.prevent="joinCrawl")
