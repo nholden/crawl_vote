aa773da75c212756702d1f265fefc3b7bcede61e
Nick Holden <nick.r.holden@gmail.com>
2018-05-23 19:38:19 -0700

    Only use dotenv npm package in development

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,5 +1,16 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
+const dotenv = require('dotenv')
+const webpack = require('webpack')
+
+const dotenvFiles = [
+  '.env',
+]
+dotenvFiles.forEach((dotenvFile) => {
+  dotenv.config({ path: dotenvFile, silent: true })
+})
+
+environment.plugins.prepend('Environment', new webpack.EnvironmentPlugin(JSON.parse(JSON.stringify(process.env))))
 
 module.exports = environment.toWebpackConfig()
