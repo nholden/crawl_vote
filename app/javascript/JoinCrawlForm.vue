69cc2092e643592f8d6e9555e57ccbcf9e6a8ff8
Nick Holden <nick.r.holden@gmail.com>
2018-06-24 17:00:43 -0700

    Provide a helpful error message when user mistypes crawl token

--- app/javascript/JoinCrawlForm.vue
+++ app/javascript/JoinCrawlForm.vue
@@ -2,7 +2,13 @@
 form(v-on:submit.prevent="joinCrawl")
   .pb-4
     label.block.pb-1.font-medium.text-sm.text-blue-light.uppercase.tracking-wide(for="token") Code
-    input.shadow.appearance-none.border.rounded.w-full.py-2.px-3(type="text" id="token" v-model="token")
+    input.shadow.appearance-none.border.rounded.w-full.py-2.px-3(
+      type="text"
+      id="token"
+      v-model="token"
+      :class="{ 'border-red': tokenNotFound }")
+    .pt-1.text-sm.text-red(v-if="tokenNotFound")
+      | Couldn’t find a crawl with that token. Check your spelling and try again.
   .pt-2
     input.btn.btn--active.w-full(type="submit" value="Join Crawl")
 </template>
@@ -11,13 +17,24 @@ form(v-on:submit.prevent="joinCrawl")
 export default {
   data: function() {
     return {
-      token: ''
+      token: '',
+      tokenNotFound: false,
     }
   },
 
   methods: {
     joinCrawl: function() {
-      window.location.href = '/crawls/' + this.token.toLowerCase()
+      var url = '/crawls/' + this.token.toLowerCase()
+
+      fetch(url).then((response) => {
+        if(response.ok) {
+          this.tokenNotFound = false
+          return window.location.href = url
+        }
+        throw new Error('Network response was not ok.')
+      }).catch((error) => {
+        this.tokenNotFound = true
+      })
     }
   }
 }
